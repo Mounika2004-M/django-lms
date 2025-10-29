ad2b11dab6952c71bd964c6edbcfc49495ae6fcf
Mounika2004-M <mouni9916@gmail.com>
2025-10-29 09:59:24 +0530

    created useradmin

---
+++ users/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=100)
+    email = serializers.EmailField(max_length=25)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from  .models import User
+from .serializers import UserSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 # Create your views here.
+
+def userData(request):
+    users = User.objects.get(id=1)
+    serializer = UserSerializer(users)
+    jsonData = JSONRenderer().render(serializer.data)
+    return HttpResponse(jsonData, content_type='application/json')
